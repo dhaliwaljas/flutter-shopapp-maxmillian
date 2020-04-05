93aed7dbbf3036a467705bbf621114d6dad8c376
dhaliwaljas <63174620+dhaliwaljas@users.noreply.github.com>
2020-04-05 02:47:18 -0700

    Section 8 - lecture 185

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 class ShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: 'ShopApp',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ShopApp'),
+      ),
+      body: Center(
+        child: Text('Nothing'),
+      ),
     );
   }
 }
