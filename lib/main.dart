f7d8b30dd4fc7b6395a895dc4c1fc664413b0a49
dhaliwaljas <63174620+dhaliwaljas@users.noreply.github.com>
2020-04-11 15:43:32 -0700

    Section 8 - lecture 204

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/Provider/cart.dart';
+import 'package:shopapp/Provider/products.dart';
+import 'package:shopapp/screens/cart_screen.dart';
+import 'package:shopapp/screens/product_detail_screen.dart';
+import 'package:shopapp/screens/products_overview_screen.dart';
+
+void main() => runApp(ShopApp());
 
 class ShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ShopApp',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ShopApp'),
-      ),
-      body: Center(
-        child: Text('Nothing'),
-      ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'ShopApp',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.tealAccent,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routName: (ctx) => CartScreen(),
+          }),
     );
   }
 }
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/Provider/cart.dart';
+import 'package:shopapp/screens/cart_screen.dart';
+import 'package:shopapp/widgets/badge.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+import 'package:shopapp/widgets/products_grid.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ShopApp'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorities'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routName);
+              },
+            ),
+          )
+        ],
+      ),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
